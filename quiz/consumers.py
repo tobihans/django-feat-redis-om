9915bc07fd876e8678d60845beaecc376a114b06
Hans Tognon <learningway403@gmail.com>
2022-11-18 19:53:56 +0100

    updates

--- quiz/consumers.py
+++ quiz/consumers.py
@@ -8,6 +8,7 @@ from channels.generic.websocket import WebsocketConsumer
 class QuizConsumer(WebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
+        self.counter_key = None
         self.quiz_pk = None
         self.room_group_name = None
 
@@ -16,16 +17,22 @@ class QuizConsumer(WebsocketConsumer):
         # Informs client of successful connection
         self.quiz_pk = self.scope["url_route"]["kwargs"]["quiz_pk"]
         self.room_group_name = f"quiz_group_{self.quiz_pk}"
+        self.counter_key = f"count:{self.room_group_name}"
 
         redis_client = redis.Redis(decode_responses=True)
-        key = f"count:{self.room_group_name}"
-        currently_connected = int(redis_client.get(key) or 0)
+        currently_connected = int(redis_client.get(self.counter_key) or 0)
 
         self.accept()
 
         self.send(
             text_data=json.dumps(
-                {"event": {"type": "join", "payload": {"count": currently_connected}}}
+                {
+                    "event": {
+                        "type": "join",
+                        "origin_channel": self.channel_name,
+                        "payload": {"count": currently_connected},
+                    }
+                }
             )
         )
 
@@ -33,7 +40,7 @@ class QuizConsumer(WebsocketConsumer):
             self.room_group_name, self.channel_name
         )
 
-        redis_client.set(key, int(currently_connected + 1))
+        redis_client.set(self.counter_key, currently_connected + 1)
 
     def disconnect(self, close_code):
         """Event when client disconnects"""
@@ -45,6 +52,10 @@ class QuizConsumer(WebsocketConsumer):
             self.room_group_name, {"type": "join", "payload": {"count": -1}}
         )
 
+        redis_client = redis.Redis(decode_responses=True)
+        currently_connected = int(redis_client.get(self.counter_key) or 0)
+        redis_client.set(self.counter_key, max(currently_connected - 1, 0))
+
     def receive(self, text_data):
         """Event when data is received"""
         text_data_json = json.loads(text_data)
@@ -55,5 +66,7 @@ class QuizConsumer(WebsocketConsumer):
         )
 
     def join(self, event):
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({"event": event}))
+        origin_channel = event.pop("origin_channel", None)
+
+        if origin_channel != self.channel_name:
+            self.send(text_data=json.dumps({"event": event}))
