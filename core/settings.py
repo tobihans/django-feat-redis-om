32c76c48c794fae19d28e50d1c26f790c4ae1cb6
Hans Tognon <learningway403@gmail.com>
2022-11-18 17:03:20 +0100

    adding channels

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
+    "channels",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -69,6 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "core.wsgi.application"
+
+ASGI_APPLICATION = "core.asgi.application"
 
 
 # Database
